4129d02b5f14853bbe113cb8879f48b0dea79e6f
ulyana frolova <6291709@gmail.com>
2026-04-01 12:14:39 +0200

    Hardcoding deleted

---
+++ Configuration/Keybinds.cs
@@ -0,0 +1,23 @@
+namespace AlchemyRPG;
+
+public static class Keybinds
+{
+    public const ConsoleKey MoveUp = ConsoleKey.W;
+    public const ConsoleKey MoveDown = ConsoleKey.S;
+    public const ConsoleKey MoveLeft = ConsoleKey.A;
+    public const ConsoleKey MoveRight = ConsoleKey.D;
+
+    public const ConsoleKey PickUp = ConsoleKey.E;
+    public const ConsoleKey Drop = ConsoleKey.X;
+    public const ConsoleKey Help = ConsoleKey.H;
+
+    public const ConsoleKey EquipBaseKey = ConsoleKey.D0; 
+    public const int EquipSlotsCount = 10;               
+    public const string EquipKeysLabel = "0-9";
+
+    public const ConsoleKey EquipLeft = ConsoleKey.Q;
+    public const ConsoleKey EquipRight = ConsoleKey.E;
+    public const ConsoleKey Attack = ConsoleKey.C;
+
+    public const ConsoleKey Cancel = ConsoleKey.Escape;
+}
---
+++ Configuration/Tiles.cs
@@ -0,0 +1,15 @@
+namespace AlchemyRPG;
+public static class Tiles
+{
+    public const char Wall = '█';
+    public const char Floor = ' ';
+
+    public const char Player = '¶';
+    public const char Unknown = '?';
+    public const char SeveralItems = '+';
+    public const char Weapon = 'W';
+
+    public const char Gold = '$';
+    public const char Coin = 'c';
+    public const char Enemy = 'E';
+}
