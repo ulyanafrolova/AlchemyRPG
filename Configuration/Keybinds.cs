9c48608be321b2f18866488187391128df08c419
ulyana frolova <6291709@gmail.com>
2026-05-30 13:19:44 +0200

    Etap 7

--- Configuration/Keybinds.cs
+++ Configuration/Keybinds.cs
@@ -10,11 +10,12 @@ public static class Keybinds
     public const ConsoleKey Drop = ConsoleKey.X;
     public const ConsoleKey Help = ConsoleKey.H;
     public const ConsoleKey Journal = ConsoleKey.J;
-    public const ConsoleKey EquipBaseKey = ConsoleKey.D0; 
-    public const int EquipSlotsCount = 10;               
+    public const ConsoleKey EquipBaseKey = ConsoleKey.D0;
+    public const int EquipSlotsCount = 10;
     public const string EquipKeysLabel = "0-9";
     public const ConsoleKey EquipLeft = ConsoleKey.Q;
     public const ConsoleKey EquipRight = ConsoleKey.R;
     public const ConsoleKey Attack = ConsoleKey.C;
     public const ConsoleKey Cancel = ConsoleKey.Escape;
+    public const ConsoleKey Insert = ConsoleKey.I;
 }
