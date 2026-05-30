b13315ea6edffc46b7ab895a7320c90d4834841c
ulyana frolova <6291709@gmail.com>
2026-05-30 22:23:14 +0200

    Etap 7

--- Client/ClientInputController.cs
+++ Client/ClientInputController.cs
@@ -9,42 +9,22 @@ namespace AlchemyRPG;
 public interface IInputState
 {
     string GetPrompt();
-    /// <summary>
-    /// Processes the key press and returns an intent DTO if the input sequence is complete.
-    /// Returns the next state of the system via the out parameter.
-    /// </summary>
-    /// <param name="key">The key pressed by the user.</param>
-    /// <param name="player">The current state of the player.</param>
-    /// <param name="nextState">The next state the context should transition to.</param>
-    /// <returns>A valid NetworkCommandDTO if an action is ready to be sent; otherwise, null.</returns>
     NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState);
 }
 
 /// <summary>
 /// Represents the context in the State design pattern.
-/// Stores the current input state and manages state transitions.
 /// </summary>
 public class ClientInputController
 {
     private IInputState _currentState;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ClientInputController"/> class.
-    /// Sets the initial state to <see cref="NormalState"/>.
-    /// </summary>
     public ClientInputController()
     {
         _currentState = new NormalState();
     }
     public string GetPrompt() => _currentState.GetPrompt();
 
-    /// <summary>
-    /// Processes a keystroke by delegating the logic to the current state,
-    /// and updates the internal state pointer for the next tick.
-    /// </summary>
-    /// <param name="key">The key pressed by the user.</param>
-    /// <param name="player">The current data transfer object representing the player.</param>
-    /// <returns>A network command DTO if an action was finalized; otherwise, null.</returns>
     public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player)
     {
         var commandDto = _currentState.ProcessInput(key, player, out IInputState nextState);
@@ -53,10 +33,6 @@ public class ClientInputController
     }
 }
 
-/// <summary>
-/// Represents the default input state where the player can move, pick up items, 
-/// or initiate multi-step actions like attacking or equipping.
-/// </summary>
 public class NormalState : IInputState
 {
     public string GetPrompt() => "";
@@ -80,9 +56,14 @@ public class NormalState : IInputState
             return null;
         }
 
+        if (key == Keybinds.Insert)
+        {
+            nextState = new WaitingForInsertSourceState();
+            return null;
+        }
+
         if (key == Keybinds.Drop)
         {
-            // Transfer control to a new state to wait for the item index argument
             nextState = new WaitingForDropItemState();
             return null;
         }
@@ -106,20 +87,12 @@ public class NormalState : IInputState
     }
 }
 
-/// <summary>
-/// Represents a state where the client is waiting for the player to select the specific type of attack.
-/// </summary>
 public class WaitingForAttackTypeState : IInputState
 {
     private readonly int _targetX;
     private readonly int _targetY;
     public string GetPrompt() => "Choose attack: [1] Normal, [2] Stealth, [3] Magic.";
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="WaitingForAttackTypeState"/> class.
-    /// </summary>
-    /// <param name="targetX">The X coordinate of the attack target.</param>
-    /// <param name="targetY">The Y coordinate of the attack target.</param>
     public WaitingForAttackTypeState(int targetX, int targetY)
     {
         _targetX = targetX;
@@ -129,40 +102,21 @@ public class WaitingForAttackTypeState : IInputState
     public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState)
     {
         nextState = new NormalState();
-
-        AttackType? attackType = key switch
+        return key switch
         {
-            ConsoleKey.D1 => AttackType.Normal,
-            ConsoleKey.D2 => AttackType.Stealth,
-            ConsoleKey.D3 => AttackType.Magic,
+            ConsoleKey.D1 => new NormalAttackCommandDTO { TargetX = _targetX, TargetY = _targetY },
+            ConsoleKey.D2 => new StealthAttackCommandDTO { TargetX = _targetX, TargetY = _targetY },
+            ConsoleKey.D3 => new MagicAttackCommandDTO { TargetX = _targetX, TargetY = _targetY },
             _ => null
-        };
-
-        if (attackType == null) 
-            return null;
-
-        return new AttackCommandDTO
-        {
-            TargetX = _targetX,
-            TargetY = _targetY,
-            AttackType = attackType.Value
         };
     }
 }
 
-/// <summary>
-/// Represents a state where the client is waiting for the player to select 
-/// which hand (left or right) to equip the chosen item into.
-/// </summary>
 public class WaitingForEquipHandState : IInputState
 {
     public string GetPrompt() => "Which hand? [Q] Left / [R] Right";
     private readonly Guid _itemId;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="WaitingForEquipHandState"/> class.
-    /// </summary>
-    /// <param name="itemId">The unique identifier of the item to be equipped.</param>
     public WaitingForEquipHandState(Guid itemId)
     {
         _itemId = itemId;
@@ -171,15 +125,15 @@ public class WaitingForEquipHandState : IInputState
     public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState)
     {
         nextState = new NormalState();
-        
+
         int handSide = key switch
         {
-            Keybinds.EquipLeft => 0,  // HandSide.Left
-            Keybinds.EquipRight => 1, // HandSide.Right
+            Keybinds.EquipLeft => 0, 
+            Keybinds.EquipRight => 1,
             _ => -1
         };
-        
-        if (handSide == -1) 
+
+        if (handSide == -1)
             return null;
 
         return new EquipCommandDTO
@@ -190,9 +144,6 @@ public class WaitingForEquipHandState : IInputState
     }
 }
 
-/// <summary>
-/// Represents a state where the client is waiting for the player to choose the physical direction of their attack.
-/// </summary>
 public class WaitingForAttackDirectionState : IInputState
 {
     public string GetPrompt() => "Attack direction? [W/A/S/D]";
@@ -218,16 +169,11 @@ public class WaitingForAttackDirectionState : IInputState
     }
 }
 
-/// <summary>
-/// Represents a state where the client is waiting for the player to select an item to drop from their inventory.
-/// </summary>
 public class WaitingForDropItemState : IInputState
 {
     public string GetPrompt() => "Drop which item? [0-9]";
     public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState)
     {
-        // Regardless of the outcome (success or invalid input), 
-        // always reset the state machine back to the default state.
         nextState = new NormalState();
 
         if (key >= Keybinds.EquipBaseKey && key < Keybinds.EquipBaseKey + Keybinds.EquipSlotsCount)
@@ -243,6 +189,49 @@ public class WaitingForDropItemState : IInputState
             };
         }
 
+        return null;
+    }
+}
+
+public class WaitingForInsertSourceState : IInputState
+{
+    public string GetPrompt() => "Insert WHICH item? [0-9]";
+    public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState)
+    {
+        nextState = new NormalState();
+        if (key >= ConsoleKey.D0 && key <= ConsoleKey.D9)
+        {
+            int index = key - ConsoleKey.D0;
+            if (index < player.Backpack.Count)
+            {
+                nextState = new WaitingForInsertTargetState(player.Backpack[index].Id);
+            }
+        }
+        return null;
+    }
+}
+
+public class WaitingForInsertTargetState : IInputState
+{
+    private readonly Guid _sourceId;
+    public WaitingForInsertTargetState(Guid sourceId) => _sourceId = sourceId;
+    public string GetPrompt() => "Insert INTO which container? [0-9]";
+
+    public NetworkCommandDTO? ProcessInput(ConsoleKey key, PlayerDTO player, out IInputState nextState)
+    {
+        nextState = new NormalState();
+        if (key >= ConsoleKey.D0 && key <= ConsoleKey.D9)
+        {
+            int index = key - ConsoleKey.D0;
+            if (index < player.Backpack.Count)
+            {
+                return new InsertCommandDTO
+                {
+                    ItemIdToInsert = _sourceId,
+                    TargetContainerId = player.Backpack[index].Id
+                };
+            }
+        }
         return null;
     }
 }
